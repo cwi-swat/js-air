7e020095f4c208d73f30a02d62d3c29a442dfa26
Jurgen Vinju <Jurgen.Vinju@cwi.nl>
2017-09-15 10:51:59 +0200

    minor cleanup

--- src/org/rascalmpl/library/lang/javascript/m3/AST.java
+++ src/org/rascalmpl/library/lang/javascript/m3/AST.java
@@ -8,65 +8,42 @@ import jdk.nashorn.internal.runtime.ScriptRuntime;
 import jdk.nashorn.internal.runtime.options.Options;
 
 public class AST {
-	String program(String file) {return "var fs = require('fs');"
-	+"var util = require('util');"
-	+"var babylon = require('babylon');"
-    +"fs.readFile(file+'.js', 'utf8', function(err, code) {"
-	+"var AST = babylon.parse(code);"
-    +"alert(AST);"
-	+ "console.log(util.inspect("
-	+    "AST,"
-	+"    false, null"
-	+ "));"
-	+ "  fs.writeFile(file+'.json', JSON.stringify(AST),"
-	+   "function(err) {"
-	+       "if (err) throw err;"
-	+       "});"
-	+"});"
-	;
-	}
-	
-	protected final IValueFactory values;
-	
-	public AST(IValueFactory values){
-		super();		
-		this.values = values;
-	}
+    // TODO: use babylon to parse the file instead of nashhorn?
+    String program(String file) {
+        return "var fs = require('fs');"
+             + "var util = require('util');"
+             + "var babylon = require('babylon');"
+             + "fs.readFile(file+'.js', 'utf8', function(err, code) {"
+             + "var AST = babylon.parse(code);"
+             + "alert(AST);"
+             +  "console.log(util.inspect("
+             +    "AST,"
+             + "    false, null"
+             + "));"
+             + "  fs.writeFile(file+'.json', JSON.stringify(AST),"
+             +   "function(err) {"
+             +       "if (err) throw err;"
+             +       "});"
+             + "});"
+            ;
+    }
 
-public String _parse(String iname, String code) { 
-    		  // values.string(ScriptUtils.parse(code.getValue(), "<unknown>", false));
-	    Options options = new Options("nashorn");
-	    ErrorManager errors = new ErrorManager();
-	    Context contextm = new Context(options, errors, Thread.currentThread().getContextClassLoader());
-	    Context.setGlobal(contextm.createGlobal());
-		return ScriptRuntime.parse(code, iname, true);
-}
+    protected final IValueFactory values;
 
-public IString _parse(IString iname, IString code) { 
-	return values.string(_parse(iname.getValue(), code.getValue()));
-}
+    public AST(IValueFactory values){
+        super();		
+        this.values = values;
+    }
 
-/*
-public String _parse(String file) { 
-	  // values.string(ScriptUtils.parse(code.getValue(), "<unknown>", false));
-	System.err.println(program(file));
-	// System.out.println(program(file.getValue()));
-	ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
-	try {
-		engine.eval(program(file));
-	} catch (ScriptException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-//return values.string(file.getValue()+".json");
-return "aap";
-}
-*/
+    public String _parse(String iname, String code) { 
+        Options options = new Options("nashorn");
+        ErrorManager errors = new ErrorManager();
+        Context contextm = new Context(options, errors, Thread.currentThread().getContextClassLoader());
+        Context.setGlobal(contextm.createGlobal());
+        return ScriptRuntime.parse(code, iname, true);
+    }
 
-
-
-static public void main(String[] argv) {
-	  AST ast = new AST(null);
-	  System.out.println(ast._parse("/noot/aap", ast.program("aap")));
-}
+    public IString _parse(IString iname, IString code) { 
+        return values.string(_parse(iname.getValue(), code.getValue()));
+    }
 }
